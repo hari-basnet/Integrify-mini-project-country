25dc5c4bdf396d4c2c13b53fc3f76e0cbe73e7f9
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-22 00:47:24 +0200

    trying to find the solution

--- js/main.js
+++ js/main.js
@@ -1,87 +1,119 @@
 // console.log(countries);
-// select the input from the html 
-const searchBox = document.querySelector('.search-box');
+// select the input from the html
+const searchBox = document.querySelector(".search-box");
 
 // target the button from html
-const buttonInitial = document.querySelector('.search-by-initials');
-const buttonAny = document.querySelector('search-by-any');
+const buttonInitial = document.querySelector(".search-by-initials");
+const buttonAny = document.querySelector(".search-by-any");
 
 // add Eventlisteners to the buttons and Input
-//buttonInitial.addEventListener('click', generateDiv);
-//buttonAny.addEventListener('click', searchCountries);
+buttonInitial.addEventListener("click", function () {
+    buttonAny.clicked == false;
+    searchBox.addEventListener("keyup", generateDiv);
+    searchBox.addEventListener("keyup", searchCountriesByInitials);
+});
 
-searchBox.addEventListener('keyup', generateDiv);
+buttonAny.addEventListener("click", function () {
+    buttonInitial.clicked == false;
+    searchBox.addEventListener("keyup", generateDiv2);
+    searchBox.addEventListener("keyup", searchCountries);
+});
 
+//console.log(buttonInitial);
+
+// searchBox.addEventListener('keyup', generateDiv);
+// searchBox.addEventListener('keyup', searchCountries);
+
+// this function filters country starting with the given searchkey
 function searchCountriesByInitials() {
     const searchKey = searchBox.value;
-    // console.log(searchKey);
-
     const searchResult = countries.filter(country => {
         return country.toUpperCase().startsWith(searchKey.toUpperCase());
-    })
-    // console.log(searchResult);
+    });
+
     return searchResult;
 }
 
-// create a function that creates div and puts the text content 
+// this function filters country from array with any searchkey
+function searchCountries() {
+    const searchKey = searchBox.value;
+    const pattern = new RegExp(searchKey, "gi");
+
+    const searchResult = countries.filter(country => {
+        return country.toUpperCase().match(pattern);
+    });
+    return searchResult;
+}
+
+//console.log(searchCountries());
+
+// make a function that chooses function
+let newCountries;
+
+// create a function that creates div and puts the text content
 
 function generateDiv() {
-
     clearItems();
     // target where you wanna put the created box
-    const resultSection = document.querySelector('.result-section');
+    const resultSection = document.querySelector(".result-section");
     //console.log(resultSection);
+    newCountries = searchCountriesByInitials();
 
-    const newCountries = searchCountriesByInitials()
-
-    const el = newCountries.forEach(element => {
-        // start by clearing the divs 
+    newCountries.forEach(element => {
+        // start by clearing the divs
         // create new divs
-        const resultDiv = document.createElement('div');
-        resultDiv.setAttribute('class', 'country-div');
+        const resultDiv = document.createElement("div");
+        resultDiv.setAttribute("class", "country-div");
         resultDiv.style.backgroundColor = generateRGB();
-        
-        const resultSpan = document.createElement('span');
+
+        const resultSpan = document.createElement("span");
         resultSpan.textContent = element;
-        
+
         // append created child to result section
         resultDiv.appendChild(resultSpan);
         resultSection.appendChild(resultDiv);
-        
     });
-    
-    
-    
 
-    
-    //console.log(resultDiv);
+    // FUNCTION GENERATEDIV ENDS
+}
 
-// FUNCTION GENERATEDIV ENDS
+function generateDiv2() {
+    clearItems();
+    // target where you wanna put the created box
+    const resultSection2 = document.querySelector(".result-section");
+    //console.log(resultSection);
+    newCountries = searchCountries();
+    newCountries.forEach(element => {
+        // start by clearing the divs
+        // create new divs
+        const resultDiv2 = document.createElement("div");
+        resultDiv2.setAttribute("class", "country-div");
+        resultDiv2.style.backgroundColor = generateRGB();
+
+        const resultSpan2 = document.createElement("span");
+        resultSpan2.textContent = element;
+
+        // append created child to result section
+        resultDiv2.appendChild(resultSpan2);
+        resultSection2.appendChild(resultDiv2);
+    });
+
+    // FUNCTION GENERATEDIV ENDS
 }
 
 // creating function that creates rgb color
-
-function generateRGB (){
-
+function generateRGB() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
 
     return `rgb(${r},${g},${b})`;
 }
-//console.log(searchCountriesByInitials());
 
-
-
-// function searchCountries() {
-
-//     console.log(8 + 8);
-// }
-
+// this function clears the search results
 function clearItems() {
-    const divsToRemove = document.querySelectorAll('.country-div');
+    const divsToRemove = document.querySelectorAll(".country-div");
     divsToRemove.forEach(element => {
         element.remove();
-    })
-
+    });
 }
