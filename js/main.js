ce4f3586444bd44d2be0dff619aeac8f78561801
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-03-25 17:37:09 +0200

    added new way for the solution, somewhat satisfying

--- js/main.js
+++ js/main.js
@@ -1,44 +1,40 @@
 // select the input from the html
 const searchBox = document.querySelector(".search-box");
-
+const buttons = document.querySelector(".buttons");
 // target the button from html
 const buttonInitial = document.querySelector(".search-by-initials");
 const buttonAny = document.querySelector(".search-by-any");
 
+buttonInitial.addEventListener('click', buttonChecker);
+buttonAny.addEventListener('click', buttonChecker);
 // add Eventlisteners to the buttons and Input
-buttonInitial.addEventListener("click", function () {
-    buttonAny.classList.remove('clicked');
-    buttonInitial.classList.add('clicked');
-    //console.log(buttonInitial);
-    searchBox.addEventListener("keyup", searchCountriesByInitials);
-    searchBox.addEventListener("keyup", generateDiv);
-});
+// buttonInitial.addEventListener("click", function () {
+//     if(onclick = true){
+//     searchBox.addEventListener("keyup", generateDiv);
+//     searchBox.addEventListener("keyup", searchCountriesByInitials);
+//     }
+    
+// });
 
-buttonAny.addEventListener("click", function () {
-    buttonInitial.clicked == false;
-    searchBox.addEventListener("keyup", generateDiv2);
-    searchBox.addEventListener("keyup", searchByAny);
-});
-
-//console.log(buttonInitial);
-
-// searchBox.addEventListener('keyup', generateDiv);
-// searchBox.addEventListener('keyup', searchByAny);
-
+// buttonAny.addEventListener("click", function () {
+//     if(onclick = true){
+//         searchBox.addEventListener("keyup", generateDiv2);
+//         searchBox.addEventListener("keyup", searchByAny);
+//     }
+// });
 // this function filters country starting with the given searchkey
 function searchCountriesByInitials() {
     const searchKey = searchBox.value;
     const searchResult = countries.filter(country => {
         return country.toUpperCase().startsWith(searchKey.toUpperCase());
     });
-
     return searchResult;
 }
 
 // this function filters country from array with any searchkey
 function searchByAny() {
     const searchKey = searchBox.value;
-    const pattern = new RegExp(searchKey, "g");
+    const pattern = new RegExp(searchKey, "gi");
 
     const searchResult = countries.filter(country => {
         return country.toUpperCase().match(pattern);
@@ -57,8 +53,7 @@ function generateDiv() {
     //console.log(resultSection);
     newCountries = searchCountriesByInitials();
 
-    countries.forEach(element => {
-        // start by clearing the divs
+    newCountries.forEach(element => {
         // create new divs
         const resultDiv = document.createElement("div");
         resultDiv.setAttribute("class", "country-div");
@@ -105,7 +100,7 @@ function generateRGB() {
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
 
-    return `rgb(${r},${g},${b})`;
+    return `rgb(${r},${g},${g})`;
 }
 
 // this function clears the search results
@@ -115,3 +110,21 @@ function clearItems() {
         element.remove();
     });
 }
+
+function buttonChecker (){
+
+    buttonInitial.classList.remove('checked');
+    buttonAny.classList.remove('checked');
+    if(buttonInitial.classList.contains('search-by-initials')){
+        buttonInitial.classList.add('checked');
+        searchBox.addEventListener("keyup", generateDiv);
+        searchBox.addEventListener("keyup", searchCountriesByInitials);
+
+    }else if (buttonAny.classList.contains('search-by-any')){
+        buttonAny.classList.add('checked');
+        searchBox.addEventListener("keyup", generateDiv2);
+        searchBox.addEventListener("keyup", searchByAny);
+    }
+}
+
+console.log(buttonChecker());
---
+++ js/test.js
@@ -0,0 +1,129 @@
+// select the input from the html
+const searchBox = document.querySelector(".search-box");
+// // target the button from html
+const buttonInitial = document.querySelector(".search-by-initials");
+const buttonAny = document.querySelector(".search-by-any");
+const resultSection = document.querySelector(".result-section");
+
+buttonInitial.addEventListener("click", function() {
+  buttonAny.classList.remove("checked");
+  this.classList.add("checked");
+  searchBox.addEventListener("keyup", searchCountriesByInitials);
+  searchCountriesByInitials();
+});
+
+buttonAny.addEventListener("click", function() {
+  buttonInitial.classList.remove("checked");
+  this.classList.add("checked");
+  searchBox.addEventListener("keyup", searchByAny);
+  searchByAny();
+});
+
+const sortButton = document.querySelector(".fas");
+sortButton.addEventListener("click", function() {
+  countries.reverse();
+  sortButton.classList.toggle("fa-sort-alpha-down");
+  sortButton.classList.toggle("fa-sort-alpha-up");
+  searchCountriesByInitials();
+  // searchByAny();
+  // sortButton.classList.add("fa-sort-alpha-down");
+});
+
+console.log(sortButton);
+
+// this function filters country starting with the given searchkey
+function searchCountriesByInitials() {
+  clearItems();
+  const searchResult = countries.filter(country => {
+    return country.toUpperCase().startsWith(searchBox.value.toUpperCase());
+  });
+
+  // target where you wanna put the created box
+  searchResult.forEach(element => {
+    // create new divs
+    const resultDiv = document.createElement("div");
+    resultDiv.setAttribute("class", "country-div");
+    resultDiv.style.backgroundColor = generateRGB();
+
+    const resultSpan = document.createElement("span");
+    resultSpan.textContent = element;
+
+    // append created child to result section
+    resultDiv.appendChild(resultSpan);
+    resultSection.appendChild(resultDiv);
+  });
+
+  // FUNCTION GENERATEDIV ENDS
+}
+
+// console.log(searchCountriesByInitials());
+
+// this function filters country from array with any searchkey
+function searchByAny() {
+  clearItems();
+  const pattern = new RegExp(searchBox.value, "gi");
+  const searchResult = countries.filter(country => {
+    return country.toUpperCase().match(pattern);
+  });
+
+  // target where you wanna put the created box
+  searchResult.forEach(element => {
+    // create new divs
+    const resultDiv = document.createElement("div");
+    resultDiv.setAttribute("class", "country-div");
+    resultDiv.style.backgroundColor = generateRGB();
+
+    const resultSpan = document.createElement("span");
+    resultSpan.textContent = element;
+
+    // append created child to result section
+    resultDiv.appendChild(resultSpan);
+    resultSection.appendChild(resultDiv);
+  });
+}
+
+// creating function that creates rgb color
+function generateRGB() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r},${g},${g})`;
+}
+
+// this function clears the search results
+function clearItems() {
+  const divsToRemove = document.querySelectorAll(".country-div");
+  divsToRemove.forEach(element => {
+    element.remove();
+  });
+}
+
+// Action listeners
+// searchBox.addEventListener("keyup", searchCountriesByInitials);
+//searchBox.addEventListener("keyup", searchByAny);
+
+//console.log(displayCountries());
+
+//console.log(displayCountries());
+
+function sortCountries() {
+  return countries.reverse();
+
+  // // target where you wanna put the created box
+  // searchResult.forEach(element => {
+  //   // create new divs
+  //   const resultDiv = document.createElement("div");
+  //   resultDiv.setAttribute("class", "country-div");
+  //   resultDiv.style.backgroundColor = generateRGB();
+
+  //   const resultSpan = document.createElement("span");
+  //   resultSpan.textContent = element;
+
+  //   // append created child to result section
+  //   resultDiv.appendChild(resultSpan);
+  //   resultSection.appendChild(resultDiv);
+  //});
+}
+
+// displayCountries();
